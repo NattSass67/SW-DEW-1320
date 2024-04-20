2bf4c5e7b43a6893047ae824976d197c47bf9b3a
NattSass67 <nattanun.sk@gmail.com>
2024-04-20 23:00:05 +0700

    fix-server

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,4 +16,22 @@ app.use(dentist);
 
 app.listen(5000, function () {
     console.log("Server PROJECT running on port 5000!");
+    console.log("BOOKING-Service-----------\n")
+    console.log('GET /api/booking');
+    console.log('GET /api/booking/:id');
+    console.log('GET /api/admin/booking');
+    console.log('POST /api/booking');
+    console.log('PUT /api/booking/:id');
+    console.log('PUT /api/admin/booking/:id');
+    console.log('DELETE /api/booking/:id');
+    console.log('DELETE /api/admin/booking/:id\n');
+    console.log("Auth-Service-----------")
+    console.log('POST /api/auth/register');
+    console.log('POST /api/auth/login\n');
+    console.log("Deentist-Service-----------")
+    console.log('GET /api/dentist');
+    console.log('GET /api/dentist/:id');
+    console.log('POST /api/dentist');
+    console.log('PUT /api/dentist/:id');
+    console.log('DELETE /api/dentist/:id');
 });
